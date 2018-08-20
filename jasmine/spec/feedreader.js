ea7144a83ac40f00368fec4df65529f156791c07
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-08-19 21:15:13 -0700

    Adds specs for allFeeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,29 +26,35 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* A test that loops through each feed in the allFeeds
+         * object and ensures it has a URL defined and that
+         * the URL is not empty.
          */
          it('have URLs defined and not empty', function() {
-
+           for(var i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].url).toBeDefined();
+             expect(allFeeds[i].url).not.toBe('');
+             expect(allFeeds[i].url).not.toBeNull();
+           }
          });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* A test that loops through each feed in the allFeeds object
+         * and ensures it has a name defined and that the name is
+         * not empty.
          */
 
          it('have names defined and not empty', function() {
-
+           for(var i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].name).toBeDefined();
+             expect(allFeeds[i].name).not.toBe('');
+             expect(allFeeds[i].name).not.toBeNull();
+           }
          });
-
     });
 
     /* New test suite named "The menu" */
     describe('The menu', function() {
-
       /* TODO: Write a test that ensures the menu element is
       * hidden by default. You'll have to analyze the HTML and
       * the CSS to determine how we're performing the
